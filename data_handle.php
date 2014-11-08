f38a237250f59f147204fe07d48bf61d5a339fc3
BowenChan <BowenmChan@gmail.com>
2014-11-08 14:57:28 -0800

    added header/Fixed minor bugs/Check Request MEthod

--- data_handle.php
+++ data_handle.php
@@ -17,32 +17,34 @@
 	
 	$selected = mysqli_select_db($link, $dbname)
 	or die("Could not select table");
-	if(isset($_POST['vid_title']) && isset($_POST['vid_link']) && isset($_POST['vid_length']) && isset($_POST['vid_resolution']) && isset($_POST['vid_descript']) && isset($_POST['vid_lang']) && isset($_POST['vid_view']) && isset($_POST['vid_type']) && isset($_POST['vid_icon']) && isset($_POST['vid_tag']))
+	if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
-		printf("Everything is filled");
-		$title = $_POST['vid_title'];
-		$videolink = $_POST['vid_link'];
-		$videolength = $_POST['vid_length'];
-		$highestresolution = $_POST['vid_resolution'];
-		$description = $_POST['vid_descript'];
-		$language = $_POST['vid_lang'];
-		$viewcount =  $_POST['vid_view'];
-		$videotype = $_POST['vid_type'];
-		$iconimage = $_POST['vid_icon'];
-		$tag = $_POST['vid_tag'];
-		if($selected){
-			
-		$SQL = "INSERT INTO `fun_video`(`title`, `videolink`, `videolength`,`highestresolution`,`description`,`language`,`viewcount`,`videotype`,`iconimage`,`tag`) VALUES ('$title', '$videolink', '$videolength','$highestresolution', '$description', '$language', '$viewcount', '$videotype', '$iconimage', '$tag')";
-		$retval = mysqli_query( $link, $SQL) or die("</br>Error");
+		if(isset($_POST['vid_title']) && isset($_POST['vid_link']) && isset($_POST['vid_length']) && isset($_POST['vid_resolution']) && isset($_POST['vid_descript']) && isset($_POST['vid_lang']) && isset($_POST['vid_view']) && isset($_POST['vid_type']) && isset($_POST['vid_icon']) && isset($_POST['vid_tag']))
+		{
+			$title = $_POST['vid_title'];
+			$videolink = $_POST['vid_link'];
+			$videolength = $_POST['vid_length'];
+			$highestresolution = $_POST['vid_resolution'];
+			$description = $_POST['vid_descript'];
+			$language = $_POST['vid_lang'];
+			$viewcount =  $_POST['vid_view'];
+			$videotype = $_POST['vid_type'];
+			$iconimage = $_POST['vid_icon'];
+			$tag = $_POST['vid_tag'];
+			if($selected){
+				
+			$SQL = "INSERT INTO `fun_video`(`title`, `videolink`, `videolength`,`highestresolution`,`description`,`language`,`viewcount`,`videotype`,`iconimage`,`tag`) VALUES ('$title', '$videolink', '$videolength','$highestresolution', '$description', '$language', '$viewcount', '$videotype', '$iconimage', '$tag')";
+			$retval = mysqli_query( $link, $SQL) or die("</br>Error");
+			}
 		}
+		else
+		{
+			printf("Not everything is filled");
+			printf("<pre>");
+			print_r($_POST);
+			printf("</pre>");
+		}	
 	}
-	else
-	{
-		printf("Not everything is filled");
-		printf("<pre>");
-		print_r($_POST);
-		printf("</pre>");
-	}	
 	
 	mysqli_close($link);
 ?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<?php
+	include('header.html');
+?>
+<body>
+</body>
+</html>
