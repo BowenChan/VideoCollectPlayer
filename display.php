d6e92d1245d4206c77579c8edb7a2eabb4552c96
BowenChan <BowenmChan@gmail.com>
2014-11-09 19:22:56 -0800

    working on header and small css

--- display.php
+++ display.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+	<link rel = "stylesheet" href = "table.css">
 <title>Untitled Document</title>
 </head>
 
@@ -16,30 +17,28 @@
 ?>
 	<table>
     	<tr>
-    	<th> id </th>
-        <th> video title </th>
-        <th> video link </th>
-        <th> video length </th>
-        <th> Highest Resolution </th>
-        <th> Description </th>
-        <th> View Count </th>
-        <th> Video Type </th>
-        <th> Icon Image </th>
-        <th> Video Tags </th>
+    	<th style = "border:none"> id </th>
+        <th style = "border:none"> video title </th>
+        <th style = "border:none"> video length </th>
+        <th style = "border:none"> Highest Resolution </th>
+        <th style = "border:none"> Description </th>
+        <th style = "border:none"> View Count </th>
+        <th style = "border:none"> Video Type </th>
+        <th style = "border:none"> Icon Image </th>
+        <th style = "border:none"> Video Tags </th>
         </tr>
 <?php
 	while($current_vid = mysqli_fetch_array($res))
 	{
-		$output = "<tr> ". 
+		$output = "<tr style=text-align:center> ". 
 		"<td>" . $current_vid['id'] . "</td>" .
 		"<td>" . $current_vid['title'] . "</td>" .
-		"<td>" . $current_vid['videolink'] . "</td>" .
 		"<td>" . $current_vid['videolength'] . " minutes</td>" .
 		"<td>" . $current_vid['highestresolution'] . "</td>" .
 		"<td>" . $current_vid['description'] . "</td>" .
-		"<td> " . $current_vid['language'] . "</td>" .
-		"<td>" . $current_vid['viewcount'] . "</td>" .
-		"<td> <img src =" . $current_vid['iconimage'] . " alt=picture></td>" .
+		"<td> " . $current_vid['viewcount'] . "</td>" .
+		"<td>" . $current_vid['language'] . "</td>" .
+		"<td><a href = " . $current_vid['videolink'] . "><img src =" . $current_vid['iconimage'] . " alt=picture></a></td>" .
 		"<td>" . $current_vid['tag'] . "</td>" .
 		"</tr>";
 		echo $output;
