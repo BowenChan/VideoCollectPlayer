8d9613c64f9f3020aff42d215f8754738837bac8
BowenChan <BowenmChan@gmail.com>
2014-11-11 20:50:35 -0800

    add text onto front page

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 	include('header.html');
 ?>
 <body>
+<h1 style = "text-align:center"> Welcome to Bowen Chan's Video Site <h1>
+<h3 style = "text-align:center"> We have video is fundamental Tai Chi and Tai Chi:Sun Style</h3>
+<h4 style = "text-align:center"> Enjoy! </h4>
 </body>
 </html>
