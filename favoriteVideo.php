ca42642d774e971646913b7a64fd23437c78f927
BowenChan <BowenmChan@gmail.com>
2014-12-01 21:13:19 -0800

    fixed display fav

--- favoriteVideo.php
+++ favoriteVideo.php
@@ -10,22 +10,22 @@
 	session_start();
 	include('connect.php');
 	include('selectdbfav.php');
-	//if(!isset($_SESSION['id'])){
-		//$userID = $_SESSION['sess_user'];
-		//$vidID = $_GET['id'];
-		//$result = mysqli_query($link, "SELECT * FROM `fav_video` WHERE `user_id`=".$userID." AND `video_id`=".$vidID);
-		//if(mysqli_num_rows($result) === 0)
-		//{
-		//	$sql = "INSERT INTO `fav_video`(`user_id`,`video_id`) VALUES ('$userID', '$vidID')";
-		//	$retval = mysqli_query($link, $sql) or die("</br>Error: " . mysqli_error($link));
+	if(isset($_SESSION['sess_user'])){
+		$userID = $_SESSION['sess_user'];
+		$vidID = $_GET['id'];
+		$result = mysqli_query($link, "SELECT * FROM `fav_video` WHERE `user_id`=".$userID." AND `video_id`=".$vidID);
+		if(mysqli_num_rows($result) === 0)
+		{
+			$sql = "INSERT INTO `fav_video`(`user_id`,`video_id`) VALUES ('$userID', '$vidID')";
+			$retval = mysqli_query($link, $sql) or die("</br>Error: " . mysqli_error($link));
 			
-		//}
+		}
 		echo $_GET['id'];
-		header('Location: '. $_SESSION['prev']);
-	//}
-	//else{
-	//	header('Location: index.php');
-	//}
+		header('Location: video.php');
+	}
+	else{
+		header('Location: index.php');
+	}
 ?>
 </body>
 </html>
